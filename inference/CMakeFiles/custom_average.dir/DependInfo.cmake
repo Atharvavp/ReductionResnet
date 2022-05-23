c215b0f558c33835286d9db2c31f0f8ba421c166
atharvavp <atharva.vijay.pawar@gmail.com>
2022-05-24 00:23:11 +0530

    Restructure Cmake

---
+++ inference/CMakeFiles/custom_average.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/atharva/Documents/ReductionResnet/inference/src/custom_average.cpp" "/home/atharva/Documents/ReductionResnet/inference/CMakeFiles/custom_average.dir/src/custom_average.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_C10D_GLOO"
+  "USE_C10D_NCCL"
+  "USE_DISTRIBUTED"
+  "USE_RPC"
+  "USE_TENSORPIPE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/atharva/anaconda3/envs/torch-nightly/lib/python3.1/site-packages/torch/include"
+  "/home/atharva/anaconda3/envs/torch-nightly/lib/python3.1/site-packages/torch/include/torch/csrc/api/include"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ inference/CMakeFiles/custom_average.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/custom_average.dir/src/custom_average.cpp.o"
+  "libcustom_average.a"
+  "libcustom_average.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/custom_average.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
